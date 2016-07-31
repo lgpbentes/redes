679d774e54dfa33bf09725eda983189654b2e433
albertosql <albertosql@hotmail.com>
2016-07-31 12:33:56 -0400

    Adicionando o projeto para o novo layout e adicionando a landing page

--- museum/backend/assets/AppAsset.php
+++ museum/backend/assets/AppAsset.php
@@ -12,9 +12,15 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/bootstrap.min.css',
         'css/site.css',
+        'css/AdminLTE.min.css',
+        'css/skins/_all-skins.min.css',
+        '//plugins/iCheck/flat/blue.css',
     ];
     public $js = [
+        'js/app.min.js',
+        'https://code.jquery.com/ui/1.11.4/jquery-ui.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- museum/frontend/views/layouts/main.php
+++ museum/frontend/views/layouts/main.php
@@ -4,13 +4,10 @@
 /* @var $content string */
 
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
-use common\widgets\Alert;
 
-AppAsset::register($this);
+$asset = AppAsset::register($this);
+$baseUrl = $asset->baseUrl
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -22,61 +19,19 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body>
+<body class="sidebar-mini skin-blue fixed">
+<!-- <body class = "sidebar-mini skin-blue-light"> -->
 <?php $this->beginBody() ?>
 
 <div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => 'Museum',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-        ['label' => 'Usuário', 'url' => ['/usuario/index']],
+    <?= $this->render('header.php',['baseUrl' => $baseUrl]) ?>
+    <?= $this->render('leftmenu.php',['baseUrl' => $baseUrl]) ?>
+    <?= $this->render('content.php',['content' => $content]) ?>
+    <?= $this->render('footer.php',['baseUrl' => $baseUrl]) ?>
 
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
+    <div class="control-sidebar-bg"></div>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
 </div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
 
 <?php $this->endBody() ?>
 </body>
