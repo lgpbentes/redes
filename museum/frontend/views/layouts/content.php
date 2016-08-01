b98aa224c49040de5318d9e7a13b8625258e67ac
Larissa Bentes <lgpbentes@gmail.com>
2016-08-01 13:39:49 -0400

    Resolvendo conflitos satanicos 2

---
+++ museum/frontend/views/layouts/content.php
@@ -0,0 +1,16 @@
+<?php
+use yii\widgets\Breadcrumbs;
+?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+    </section>
+
+    <!-- Main Content -->
+    <section class="content">
+        <?= $content ?>
+    </section> <!-- /.content -->
+</div> <!-- /.content-wrapper -->
