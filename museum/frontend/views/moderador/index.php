ee4e009a2d04ae4d6fb760b80ca1ef043e7b56bc
Larissa Bentes <lgpbentes@gmail.com>
2016-08-03 16:56:46 -0400

    Adicionada tela de moderação de histórias

--- museum/frontend/views/moderador/index.php
+++ museum/frontend/views/moderador/index.php
@@ -1,33 +1,96 @@
 <?php
+use frontend\models\Historia;
+use common\models\User;
 
-use yii\helpers\Html;
-use yii\grid\GridView;
-use yii\widgets\Pjax;
 /* @var $this yii\web\View */
-/* @var $searchModel frontend\models\ModeradorSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Moderadors';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Museum';
 ?>
-<div class="moderador-index">
+<div class="site-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Create Moderador', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-<?php Pjax::begin(); ?>    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
 
-            'login',
-            'senha',
+    <!-- Box Header -->
+    <?php
+    $historias = Historia::find()->all();
+    foreach ($historias as $id => $hist){
+        //var_dump($hist);
 
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-<?php Pjax::end(); ?></div>
+        $numHistoria = $hist->id;
+        //procura pelo username do autor da postagem
+        $autor = User::findOne($hist->autor)->username;
+        $caminhoImagem= "/redes/museum/".$hist->imagem;
+        $tempo = $hist->duracao;
+        $titulo = $hist->nome;
+        $qteViews = $hist->qteViews;
+        $qteLikes = $hist->qteGostei;
+        $qteDeslikes = $hist->qteNaoGostei;
+        $descricao = $hist->descricao;
+
+        if($hist->status == 0){
+            ?>
+            <div class="col-md-4">
+                <div class="box box-widget">
+                    <div class="box-header with-border">
+
+                        <div class="user-block">
+                            <img class="img-circle" src="img/perfiltmp.png" alt="Imagem do usuário">
+                            <span class="username"><a href="#"><?=$autor?></a></span>
+                            <span class="description">Tempo do relacionamento: <?= $tempo ?> dias</span>
+                        </div>
+
+                        <div class="box-tools">
+                            <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                        </div>
+
+                    </div>
+
+                    <!-- box-body -->
+                    <div class="box-body" style="display: block;">
+                        <img class="img-responsive pad" src="<?= $caminhoImagem ?>" alt="Photo">
+                        <p><?=$titulo?></p>
+                        <p><?=$descricao?></p>
+
+                        <button type="button" class="btn btn-success btn-sm"><i class="fa fa-check"></i> </button>
+                        <button type="button" class="btn btn-danger btn-sm"><i class="fa fa-close"></i> </button>
+
+                        <span class="pull-right text-muted"><?=$qteViews?> views -  <?=$qteLikes?> likes - <?= $qteDeslikes ?> dislikes</span>
+
+                    </div><!-- /.box-body -->
+
+                </div>
+            </div>
+            <?php
+        }
+    }
+    ?>
+</div>
+
+<script>
+    var historiaclicada;
+    function detalhar(id) {
+        historiaclicada = id;
+        var titulo, descricao;
+        titulo = document.querySelector("#detalheTitulo");
+        descricao = document.querySelector("#detalheHistoria");
+
+
+        $.get('index.php?r=site/detalhes&id='+id,function (dados) {
+            dados = JSON.parse(dados);
+
+            titulo.innerHTML = dados.titulo;
+            descricao.innerHTML= dados.descricao;
+
+        });
+    }
+
+    function like() {
+        $.get('index.php?r=site/like&id='+historiaclicada,function (dados) {});
+    }
+
+    function deslike() {
+        $.get('index.php?r=site/deslike&id='+historiaclicada,function (dados) {
+        });
+    }
+
+</script>
--- processa.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo $_GET['nome'] . "\n";
-echo $_GET['sexo'] . "\n";
-echo $_GET['comentarios'] . "\n";
